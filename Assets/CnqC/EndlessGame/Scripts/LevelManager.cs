e342e5a9c360d47949000a5547ddd147390f7376
CnqC <130760728+CnqC@users.noreply.github.com>
2023-06-08 19:17:04 +0700

    LevelManager Complete
    LevelManager Complete

--- Assets/CnqC/EndlessGame/Scripts/LevelManager.cs
+++ Assets/CnqC/EndlessGame/Scripts/LevelManager.cs
@@ -35,15 +35,22 @@ public class LevelManager : MonoBehaviour, ISingleTon
 
             if (level == null) continue; // nếu mà level == null thì sẽ ngắt vòng lập và chuyển sang vòng lập khác
 
+            string levelDataKey = GamePref.LevelUnlocked.ToString() + i;
+
             if(i == 0 ) // mở khóa level đầu
             {
 
+                Pref.SetLevelUnlock(i, true); // truyền vào cho levelId là biến đếm là i, trạng thái mở khóa là true
             }
             else
             {
                 // nếu dữ liệu chưa được lưu xuống máy người chơi thì sẽ lưu dữ liệu.
                 // tức là khóa các level khác lại.
 
+                if(!PlayerPrefs.HasKey(levelDataKey)) // nếu mà dữ liệu chưa được lưu xuống máy người dùng
+                {
+                    Pref.SetLevelUnlock(i, false);
+                }
 
             }
             
@@ -55,6 +62,18 @@ public class LevelManager : MonoBehaviour, ISingleTon
     {
         
     }
+    
+    public LevelItem GetLevel()
+    {
+        if(levels != null && levels.Length <= 0)
+        {
+            // trả về mảng levels và lấy ra chỉ số bằng các chỉ số của level mà mình đã lưu dưới máy người dùng
+            return levels[Pref.CurLevelId];
+        }
+
+        return null;  // null khi = 0 hay 0 có phần tử 
+    }
+
 
     public void MakeSingleTon()
     {
