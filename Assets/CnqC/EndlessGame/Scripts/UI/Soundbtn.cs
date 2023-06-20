96686c9085a99967a03080f3fa9a99f953c787f5
CnqC <130760728+CnqC@users.noreply.github.com>
2023-06-20 12:44:32 +0700

    Create SoundBtnCripts and and it for button in game
    Create SoundBtnCripts and and it for button in game

---
+++ Assets/CnqC/EndlessGame/Scripts/UI/Soundbtn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CnqC.EndLessGame;
+using UnityEngine.UI;
+
+
+public class Soundbtn : MonoBehaviour
+{
+    private Button m_btn;
+
+
+
+    private void Awake()
+    {
+        m_btn = GetComponent<Button>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (m_btn == null) return;
+
+        m_btn.onClick.RemoveAllListeners();
+        // tạo sự kiện cho btn này khi được click sẽ tạo ra hiêu ứng playsound
+        m_btn.onClick.AddListener(() => PlaySound());
+    }
+
+    private void PlaySound()
+    {
+        if (AudioController.Ins == null) return;
+
+        AudioController.Ins.PlaySound(AudioController.Ins.btnClick);
+    }
+}
